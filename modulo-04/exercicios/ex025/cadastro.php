d8b4021066a572eb60a5a0c9ac6f00fbdfb9dc1e
Gabriel Said <72895033+gabrielsousasaid@users.noreply.github.com>
2025-07-24 14:53:25 -0300

    criado o form002

---
+++ modulo-04/exercicios/ex025/cadastro.php
@@ -0,0 +1,8 @@
+<?php 
+
+# Aqui criamos uma página PHP atrelada ao action do form001 para parar de aparecer a msg de erro
+
+?>
+
+Os Dados foram enviados!
+Aprenda PHP para saber o que fazer com eles
